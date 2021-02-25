ee78d123f0649a9fb955fcb574f4eaec76f21047
michaelionia <ioniamichael@gmail.com>
2021-02-25 18:04:17 +0200

    Added colors.

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,9 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {LoginScreen} from './screens/LoginScreen';
-import {SignUpScreen} from './screens/SignUpScreen';
+import {LoginScreen} from './screens/auth/LoginScreen';
+import {SignUpScreen} from './screens/auth/SignUpScreen';
 import {SplashScreen} from './screens/SplashScreen';
-import {HomeScreen} from './screens/HomeScreen';
+import {HomeScreen} from './screens/main/HomeScreen';
 
 const AuthStack = createStackNavigator({
     LoginScreen,
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+    YELLOW: '#f8d47e',
+    TEXT_COLOR: '#504a63',
+    GREY: '#f5f8fb',
+    WHITE: '#fff',
+}
