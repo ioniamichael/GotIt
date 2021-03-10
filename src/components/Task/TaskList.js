62abd1700d6c09e8d771779efd295dfee02ffa87
michaelionia <ioniamichael@gmail.com>
2021-03-10 19:09:36 +0200

    Added SectionList and that sorted by dates.

--- src/components/Task/TaskList.js
+++ src/components/Task/TaskList.js
@@ -1,26 +1,60 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, View, StyleSheet, Text} from 'react-native';
+import {FlatList, View, StyleSheet, Text, SectionList} from 'react-native';
 import {TaskCard} from './TaskCard';
+import moment from 'moment';
+
 
 export const TaskList = ({data, onTaskPress, onTaskLongPress}) => {
 
+    const sortedData = [
+        {
+            title: 'Today',
+            data: data.filter((task) => moment().format('YYYY-MM-DD') === moment(task.taskEndDate).format('YYYY-MM-DD'))
+        },
+        {
+            title: 'Tomorrow',
+            data: data.filter((task) => moment().add(1, 'd').format('YYYY-MM-DD') === moment(task.taskEndDate).format('YYYY-MM-DD'))
+        },
+        {
+            title: moment().add(2, 'd').format('YYYY-MM-DD'),
+            data: data.filter((task) => moment().add(2, 'd').format('YYYY-MM-DD') === moment(task.taskEndDate).format('YYYY-MM-DD'))
+        },
+        {
+            title: 'Future',
+            data: data.filter((task) => moment().add(2, 'd').format('YYYY-MM-DD') < moment(task.taskEndDate).format('YYYY-MM-DD'))
+        }
+    ];
+
+    console.log(moment().add(1, 'd').format('YYYY-MM-DD'));
+
+    const renderTitleIfHasData = (dataToRender, titleToRender) => {
+        if (dataToRender.length){
+            return(
+                <Text>{titleToRender}</Text>
+            )
+        }
+    };
 
     return (
-        <FlatList
-            data={data.sort((a, b) => a.taskEndDate.toString().localeCompare(b.taskEndDate.toString()))}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(task, index) => 'D' + index.toString()}
+        <SectionList
+            sections={sortedData}
+            keyExtractor={(item, index) => item + index}
             renderItem={({item, index}) => {
-                return (
-                    <View style={styles.container}>
-                        <TaskCard data={item} index={index} onTaskPress={onTaskPress}
-                                  onTaskLongPress={onTaskLongPress}/>
-                    </View>
-                );
-            }}/>
+                return(
+                    <TaskCard data={item} index={index} onTaskPress={onTaskPress} onTaskLongPress={onTaskLongPress} />
+                )
+            }}
+            renderSectionHeader={({section: {title}, section:{data}})=>{
+                return(
+                    renderTitleIfHasData(data, title)
+                )
+            }}
+        />
     );
+
 };
 
 const styles = StyleSheet.create({
     container: {},
 });
+
